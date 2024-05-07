01a6b2e69221adbc49470a8484ec5a63fe9866fb
gunjangaur <gunjangaur04@gmail.com>
2024-05-07 12:53:34 +0530

    Welcome Controller

---
+++ src/main/java/com/eazybytes/springbootbasic/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.eazybytes.springbootbasic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayWelcome(){
+        return "Welcome to spring boot learning";
+    }
+}
